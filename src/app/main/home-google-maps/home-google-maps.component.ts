71c1c10297bb9e64fc21606ca7dad71d34ad42a1
Victor Gonzalez <victor.gonzalez@metricsfab.com>
2020-11-11 20:22:33 -0600

    Se agrega el servicio home

--- src/app/main/home-google-maps/home-google-maps.component.ts
+++ src/app/main/home-google-maps/home-google-maps.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { position } from 'esri/widgets/CoordinateConversion/support/Conversion';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-home-google-maps',
@@ -13,6 +15,10 @@ export class HomeGoogleMapsComponent implements OnInit, AfterViewInit {
   lat = 40.730610;
   lng = -73.935242;
   coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  // Marcadores
+  markers: google.maps.Marker[];
+
   mapOptions: google.maps.MapOptions = {
     center: this.coordinates,
     zoom: 8,
@@ -22,14 +28,32 @@ export class HomeGoogleMapsComponent implements OnInit, AfterViewInit {
     map: this.map,
   });
 
-  constructor() { }
+  constructor(private homeService: HomeService) { }
 
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
     this.marker.setMap(this.map);
+
+    this.homeService.getVehicles()
+    .subscribe( {
+      next: (vehicles) => {
+
+        this.markers = vehicles.map( vehicle => {
+          const newMarker = new google.maps.Marker();
+          const latlng = new google.maps.LatLng(40.73061, 73.935242);
+          newMarker.setPosition(latlng);
+          newMarker.setMap(this.map);
+          newMarker.setIcon('/assets/images/svg/car.svg');
+          return newMarker;
+
+            });
+      }
+    });
+
    }
 
   ngOnInit(): void {
+    
   }
 
 
