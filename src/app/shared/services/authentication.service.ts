7aa5b970c7dcc127ab397de6e6f0a6d2021b4387
Victor Gonzalez <victor.gonzalez@metricsfab.com>
2021-09-24 13:41:15 -0500

    Implments IAuthenticationService to call methods inside utils library

--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -4,13 +4,35 @@ import { catchError, map } from 'rxjs/operators';
 import { DataService, DataServiceProtocols, StorageService, StorageType } from 'utils';
 import { AppConfigService } from './app-config.service';
 import { AuthenticatedUser } from '../models/authenticated-user.model';
+import { IAuthenticationService } from 'utils';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
+export class AuthenticationService implements IAuthenticationService {
 
   constructor(private appConfigService: AppConfigService, private dataService: DataService, private storageService: StorageService) { }
+  
+  /**
+   * 
+   * @returns El usuario autenticado
+   */
+  getUser() {
+    const user = this.storageService.retrieve(this.appConfigService.currentUserKey, StorageType.Session);
+    if(user !== null && user !== undefined)
+    {
+        return JSON.parse(user);
+    }
+    return null;
+  }
+
+  /**
+   * Remove
+   */
+  logOut(): void {
+       // remove user from local storage to log user out
+        this.storageService.removeItem(this.appConfigService.currentUserKey, StorageType.Session);
+  }
 
   /**
    * 
@@ -21,7 +43,7 @@ export class AuthenticationService {
    */
   public login(username: string, password: string, lang: string) {
 
-    return this.dataService.post(this.appConfigService.logiUrl + '/auth', { username, password }, 'login', DataServiceProtocols.HTTPS)
+    return this.dataService.post(this.appConfigService.apiUrl + '/auth', { username, password }, 'login', DataServiceProtocols.HTTPS)
     .pipe(catchError( error => throwError(error)), map(result => {
       const user: AuthenticatedUser = { username, token: result.token };
       this.storageService.store(this.appConfigService.currentUserKey, JSON.stringify(user), StorageType.Session);
