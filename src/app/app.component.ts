e7984ab518d4f2b3f450cb672dbb67e014c85555
Victor Gonzalez <victor.gonzalez@metricsfab.com>
2021-10-07 15:06:29 -0500

    added getAppConfig call for get app configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UtilsService } from 'utils';
 import {AppConfigService} from './shared/services/app-config.service';
+import { AuthenticationService } from './shared/services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,13 @@ export class AppComponent {
   title = 'ubimp';
 
   // Constructor de la clase
-  constructor(private utilsService: UtilsService, appConfigService: AppConfigService) {
+  constructor(private utilsService: UtilsService, appConfigService: AppConfigService, private authenticationService: AuthenticationService) {
+    // Gets the app configuration, this includes the api url, the realtime url. You can access
+    // to the loaded configuration thrown the methods from the appConfigService
+    appConfigService.getAppConfig();
 
-    utilsService.configureUtils('home', 'login', appConfigService.currentUserKey);
+    
+    this.utilsService.configureUtils('home', 'login', appConfigService.currentUserKey, authenticationService);
+    this.utilsService.reloadPageOnHttpNoAuthorizedError = false;
   }
 }
