63cc58e83fd00087d7b68e4cf2e6f94626a80063
Victor Gonzalez <victor.gonzalez@metricsfab.com>
2021-11-18 14:21:35 -0600

    Object types for different vehicles

---
+++ src/app/main/settings/models/object-types.model.ts
@@ -0,0 +1,13 @@
+export interface ObjectType {
+
+    /**
+     * Tag to be used in the translate service to display the name acording to the current language
+     */
+    displayNameTag: string;
+
+    /**
+     * Ui identifier of the object
+     */
+    uiId: number;
+
+}
