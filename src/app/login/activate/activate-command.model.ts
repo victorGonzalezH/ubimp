8ef7eba427b0ebbd7f14c009370dad1e944122cf
Victor <victor.gonzalez.hernandez.10@gmail.com>
2022-06-16 13:52:33 -0500

    Added activate command

---
+++ src/app/login/activate/activate-command.model.ts
@@ -0,0 +1,13 @@
+export interface ActivateCommand {
+
+    /**
+     * Token to be used to activate the user account
+     */
+    token: string;
+
+    /**
+     * User language
+     */
+    lang: string;
+
+}
