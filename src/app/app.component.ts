d202382105648f3be84cff4617140added16ce93
Victor Gonzalez <victor.gonzalez@metricsfab.com>
2022-06-16 15:33:42 -0500

    Removed init configuration to be moved to the initializar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { UtilsService } from 'utils';
+import { StorageService, UtilsService } from 'utils';
 import {AppConfigService} from './shared/services/app-config.service';
 import { AuthenticationService } from './shared/services/authentication.service';
 
@@ -13,13 +13,7 @@ export class AppComponent {
   title = 'ubimp';
 
   // Constructor de la clase
-  constructor(private utilsService: UtilsService, appConfigService: AppConfigService, private authenticationService: AuthenticationService) {
-    // Gets the app configuration, this includes the api url, the realtime url. You can access
-    // to the loaded configuration thrown the methods from the appConfigService
-    appConfigService.getAppConfig();
-
+  constructor(private utilsService: UtilsService, appConfigService: AppConfigService, private authenticationService: AuthenticationService, private storageService: StorageService) {
     
-    this.utilsService.configureUtils('home', 'login', appConfigService.currentUserKey, authenticationService);
-    this.utilsService.reloadPageOnHttpNoAuthorizedError = false;
   }
 }
